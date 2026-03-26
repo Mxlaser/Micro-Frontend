ce9b1a01a1f700f3e9250e57b171fa35c59826e8
Mxlaser <66446663+Mxlaser@users.noreply.github.com>
2026-03-26 09:37:50 +0000

    feat: configuration initiale du shell

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,7 @@
 import React from 'react';
-import './App.css';
 
-function App() {
-  return (
-    <div className="shell">
-      <header className="shell-header">
-        <h1>PixelArena</h1>
-        <p>Shell operationnel</p>
-      </header>
-
-      <main className="shell-content">
-        <div className="placeholder">
-          <p>Les Micro-Frontends seront charges ici</p>
-        </div>
-      </main>
-
-      <footer className="shell-footer">
-        <p>Module Federation actif sur le port 3000</p>
-      </footer>
-    </div>
-  );
-}
+const App = () => {
+  return <h1>Shell OK</h1>;
+};
 
 export default App;
